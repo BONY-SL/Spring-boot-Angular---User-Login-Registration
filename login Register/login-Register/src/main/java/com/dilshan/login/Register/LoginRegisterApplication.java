05c17118031a387887beb08b3be92be97e1b8e2b
DILSHAN PATHIRAGE <143308037+BONY-SL@users.noreply.github.com>
2024-08-16 16:54:35 +0530

    create entity

--- login Register/login-Register/src/main/java/com/dilshan/login/Register/LoginRegisterApplication.java
+++ login Register/login-Register/src/main/java/com/dilshan/login/Register/LoginRegisterApplication.java
@@ -2,8 +2,10 @@ package com.dilshan.login.Register;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class LoginRegisterApplication {
 
 	public static void main(String[] args) {
