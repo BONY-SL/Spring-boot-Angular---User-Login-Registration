832cb8fb04f5f6508a4ca9115084ea87208b7119
DILSHAN PATHIRAGE <143308037+BONY-SL@users.noreply.github.com>
2024-08-16 20:18:32 +0530

    mail service and jwt authentication

--- login Register/login-Register/src/main/java/com/dilshan/login/Register/LoginRegisterApplication.java
+++ login Register/login-Register/src/main/java/com/dilshan/login/Register/LoginRegisterApplication.java
@@ -3,9 +3,11 @@ package com.dilshan.login.Register;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableAsync
 public class LoginRegisterApplication {
 
 	public static void main(String[] args) {
