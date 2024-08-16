0cfcc4e5521fdb3cf505f4cfa247e1ef8991d669
DILSHAN PATHIRAGE <143308037+BONY-SL@users.noreply.github.com>
2024-08-17 01:18:30 +0530

    ui

---
+++ login Register/loginregisterui/src/app/active-account/active-account.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-active-account',
+  templateUrl: './active-account.component.html',
+  styleUrl: './active-account.component.css'
+})
+export class ActiveAccountComponent {
+
+}
